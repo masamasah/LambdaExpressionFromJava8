0742a79ddd3ae84867cc9ac0b0962c1b21cef303
masayuki.hirata <masayuki.h0729@gmail.com>
2019-01-03 18:07:21 +0900

    1-1
    SimpleRunnableを実行した

---
+++ src/com/hrt/SimpleRunnable.java
@@ -0,0 +1,9 @@
+package com.hrt;
+
+public class SimpleRunnable {
+
+    public static void main(String[] args){
+        Runnable rn = () -> System.out.println("ラムダ式");
+        rn.run();
+    }
+}

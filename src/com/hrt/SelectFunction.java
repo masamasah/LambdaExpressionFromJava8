f63ce39bc36d7e1ed4cccb450ddd2d111ff80d3a
masayuki.hirata <masayuki.h0729@gmail.com>
2019-01-05 15:31:27 +0900

    1-17
    新たな式を追加した

--- src/com/hrt/SelectFunction.java
+++ src/com/hrt/SelectFunction.java
@@ -7,12 +7,24 @@ public class SelectFunction {
         Runnable r0 = () -> System.out.println("有効な関数名を指定してください");
         Runnable r1 = () -> System.out.println("関数r1です");
 
+        Runnable r2 = () -> {
+            String name = "ゲスト";
+
+            if(args.length >= 2){
+                name = args[1];
+            }
+            System.out.println("関数r2へようこそ、"+name+"さん");
+        };
+
         if (args.length <= 0) {
             r0.run();
         } else {
             switch (args[0]) {
                 case "r1":
                     r1.run();
+                    break;
+                case "r2":
+                    r2.run();
                     break;
                 default:
                     r0.run();

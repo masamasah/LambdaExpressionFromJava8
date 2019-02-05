39f9e7c47ab870c0f60755838f06be9088a91124
masayuki.hirata <masayuki.h0729@gmail.com>
2019-02-05 19:43:20 +0900

    5-32
    Predicateのデフォルトメソッドを試してみた

--- src/com/hrt/HistoryPersonSearch.java
+++ src/com/hrt/HistoryPersonSearch.java
@@ -1,6 +1,7 @@
 package com.hrt;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class HistoryPersonSearch {
@@ -8,8 +9,22 @@ public class HistoryPersonSearch {
     public static void main(String[] args) {
         List<HistoryPerson> people = HistoryPerson.listPeople();
 
-        if (args.length > 0) {
-            // TODO: implement
+        if (args.length > 0) {z
+            int argYear = Integer.parseInt(args[0]);
+
+            Predicate<HistoryPerson> isborn = (person) -> person.getBornYear() < argYear;
+            Predicate<HistoryPerson> notdead = (person) -> person.getDeadYear() > argYear;
+
+            Consumer<HistoryPerson> write = (p) -> System.out.println(p.getName()+"は"+(argYear-p.getBornYear()+"歳でした"));
+
+            System.out.println(argYear+"年には");
+
+            for(HistoryPerson p: people){
+                if(isborn.and(notdead).test(p)){
+                    write.accept(p);
+                }
+            }
+
         } else {
             Predicate<HistoryPerson> okehazama = (person) -> person.getBornYear() < 1560;
             for (HistoryPerson p : people) {

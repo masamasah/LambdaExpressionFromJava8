c0250697a2b6009f4d424ccd09f2c4f5fd9cc632
masayuki.hirata <masayuki.h0729@gmail.com>
2019-02-06 21:25:45 +0900

    6-4
    とても古い書き方

--- src/com/hrt/StreamTest.java
+++ src/com/hrt/StreamTest.java
@@ -1,17 +1,28 @@
 package com.hrt;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class StreamTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<HistoryPerson> people = HistoryPerson.listPeople();
 
-        if(args.length > 0){
+        if (args.length > 0) {
+            switch (args[0]) {
+                case "veryold":
+                default:
+                    System.out.println("とても古い書き方");
 
-        }
-        else
-        {
-            for(HistoryPerson p: people){
+                    Iterator<HistoryPerson> it = people.iterator();
+
+                    while (it.hasNext()) {
+                        HistoryPerson p = it.next();
+                        System.out.println(p.getName());
+                    }
+                    break;
+            }
+        } else {
+            for (HistoryPerson p : people) {
                 System.out.println(p.getName());
             }
         }

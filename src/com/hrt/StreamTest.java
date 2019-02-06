6b566b33809bd46bd9e85a8b78025b773b674378
masayuki.hirata <masayuki.h0729@gmail.com>
2019-02-06 21:20:45 +0900

    6-1
    通常のfor表記

--- src/com/hrt/StreamTest.java
+++ src/com/hrt/StreamTest.java
@@ -1,4 +1,19 @@
 package com.hrt;
 
+import java.util.List;
+
 public class StreamTest {
+    public static void main(String[] args){
+        List<HistoryPerson> people = HistoryPerson.listPeople();
+
+        if(args.length > 0){
+
+        }
+        else
+        {
+            for(HistoryPerson p: people){
+                System.out.println(p.getName());
+            }
+        }
+    }
 }

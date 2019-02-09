de57c9104907d998ebfa327718ca8cd51975e072
masayuki.hirata <masayuki.h0729@gmail.com>
2019-02-09 15:04:38 +0900

    6-23
    collectを試した

--- src/com/hrt/CustomerSearch.java
+++ src/com/hrt/CustomerSearch.java
@@ -1,6 +1,7 @@
 package com.hrt;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomerSearch {
 
@@ -19,6 +20,22 @@ public class CustomerSearch {
                     customers.stream()
                             .sorted((c1, c2) -> c1.getName().compareTo(c2.getName()))
                             .forEach(c -> System.out.println(c.getName()));
+                    break;
+                case "collect":
+                    List<Customer> men = customers.stream().filter(c -> c.getGender() == Gender.MAN)
+                            .collect(Collectors.toList());
+
+                    List<Customer> women = customers.stream().filter(c -> c.getGender() == Gender.WOMAN)
+                            .collect(Collectors.toList());
+
+                    System.out.println("女性一覧");
+                    women.stream().sorted((c1, c2) -> c1.getName().compareTo(c2.getName()))
+                            .forEach((c) -> System.out.println(c.getName()));
+
+                    System.out.println("男性一覧");
+                    men.stream().sorted((c1, c2) -> c1.getName().compareTo(c2.getName()))
+                            .forEach((c) -> System.out.println(c.getName()));
+                    break;
             }
         } else {
             customers.forEach(CustmerPrinter::printCustomer);
